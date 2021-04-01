cbb27c6f3eb08c350ab51ec6e0fdf2d92d77c832
Thicc-Boi01 <tjc27882@gmail.com>
2021-04-01 15:20:05 +1100

    fizz

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        for (int Number = 1; Number <= 100; Number++) {
+            if (Number % 3 == 0 && Number % 5 == 0) 
+            {
+               Console.WriteLine("FizzBuzz");
+            }
+            if (Number % 3 == 0) 
+            {
+               Console.WriteLine("Fizz");
+            }
+            if (Number % 5 == 0) 
+            {
+               Console.WriteLine("Buzz");
+            }
+               Console.WriteLine(Number);
+            }
+        }
+    }
+}
